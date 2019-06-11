cd09d329e3f60429a20fb115a8cb9e2fc295940b
Emily Bolten <emilybolten@gmail.com>
2019-06-11 15:00:03 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create(first_name:'Emily',last_name:'Bolten')
+Student.create(first_name:'Sam',last_name:'Camacho')
